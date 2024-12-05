bc7727c65e59cf88f54972809e21b77230662f58
genettialexandrs00029 <genettialexandro@gmail.com>
2024-12-05 13:33:48 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://biuoninurigsi.xyz'
+	const proxyUrl = 'https://niuehubsolikijbert.buzz'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
